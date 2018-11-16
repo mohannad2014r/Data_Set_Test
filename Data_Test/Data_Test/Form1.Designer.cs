6108ca2f91383f792ebe9c6314d0c3b0e4bc549f
mohannad2014r <45021751+mohannad2014r@users.noreply.github.com>
2018-11-16 14:38:47 +0000

    Add files via upload

---
+++ Data_Test/Data_Test/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Data_Test
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.gv = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.gv)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(340, 401);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "View_Data";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// gv
+			// 
+			this.gv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.gv.Location = new System.Drawing.Point(22, 12);
+			this.gv.Name = "gv";
+			this.gv.Size = new System.Drawing.Size(766, 383);
+			this.gv.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.gv);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.gv)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.DataGridView gv;
+	}
+}
+
---
+++ Data_Test/Data_Test/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace Data_Test
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			DataTable dt = new DataTable();
+			using (System.IO.TextReader tr = File.OpenText((@"Data.pre")))
+			{
+				string line;
+				//add new list of string arrey
+				List<string[]> lststr = new List<string[]>();
+				while ((line = tr.ReadLine()) != null)
+				{
+
+					string[] items = Regex.Split(line,"=");
+					lststr.Add(items);
+				}
+				int col = lststr.Max(x => x.Length);
+				if (dt.Columns.Count == 0)
+				{
+					// Create the data columns for the data table based on the number of items
+					// on the first line of the file
+					for (int i = 0; i < col; i++)
+						dt.Columns.Add(new DataColumn("Column" + i, typeof(string)));
+				}
+				// loop the list 
+				foreach (string[] item in lststr)
+				{
+					dt.Rows.Add(item);
+				}
+				gv.DataSource = dt;
+
+				//set autosize mode
+				gv.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+				gv.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+				gv.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+				//datagrid has calculated it's widths so we can store them
+				for (int i = 0; i <= gv.Columns.Count - 1; i++)
+				{
+					//store autosized widths
+					int colw = gv.Columns[i].Width;
+					//remove autosizing
+					gv.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+					//set width to calculated by autosize
+					gv.Columns[i].Width = colw;
+				}
+			}
+			//show it in gridview 
+			this.gv.DataSource = dt;
+		}
+	}
+}
